3553a142fa13b65f820faeb82c60d3868ae344da
jvilla <josevillacinda96@gmail.com>
2021-04-22 18:56:59 -0700

    added test text for all pages

---
+++ src/Pages/Save/Save.js
@@ -0,0 +1,8 @@
+const Save = () => {
+  return(
+    <>
+    <h1>This is the Save Page</h1>
+    </>
+  )
+}
+export default Save
---
+++ src/Pages/Search/Search.js
@@ -0,0 +1,7 @@
+const Search = () => {
+  return(
+    <>
+  <h1>This is the Search page</h1>
+    </>
+  )
+}
